5aef344796ca72c4a27763e615b06b80c8989d6e
Ian McInerney <i.mcinerney17@imperial.ac.uk>
2023-06-06 17:58:59 +0100

    OSQP_CUDA_jll build 100.0.0+0

--- src/OSQP_CUDA_jll.jl
+++ src/OSQP_CUDA_jll.jl
@@ -2,6 +2,8 @@
 baremodule OSQP_CUDA_jll
 using Base
 using Base: UUID
+using LazyArtifacts
+Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("OSQP_CUDA")
